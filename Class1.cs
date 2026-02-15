538758900b9d00589f16a5c0267396e6f6fda38e
Esmeralda Soto <esmeraldatsoto@gmail.com>
2026-02-14 20:08:39 -0400

    Commit 2

---
+++ Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+// Clase simple que representa una persona
+class Persona
+{
+    // Propiedades públicas
+    public string Nombre { get; set; }
+    public int Edad { get; set; }
+
+    // Constructor para inicializar valores
+    public Persona(string nombre, int edad)
+    {
+        Nombre = nombre;
+        Edad = edad;
+    }
+
+    // Método que imprime una presentación
+    public void Presentarse()
+    {
+        Console.WriteLine($"Hola, mi nombre es {Nombre} y tengo {Edad} años.");
+    }
+}
